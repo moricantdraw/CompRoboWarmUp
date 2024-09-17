4a037dbf0752a9d5c82c445b24e3e10542b17b84
moricantdraw <mchevere@olin.edu>
2024-09-17 19:42:14 -0400

    works in sim

--- warmup_project/teleop.py
+++ warmup_project/teleop.py
@@ -12,44 +12,49 @@ class TeleopNode(Node):
 
     def __init__(self):
         super().__init__("teleop_node")
-        self.publisher = self.create_publisher(Twist,'cmd_vel', 10)
+        self.create_timer(0.1, self.run_loop)
+        self.vel_pub = self.create_publisher(Twist,'cmd_vel', 10)
     
-    def drive():
+    def direction(self, key):
         vel = Twist()
         if key == 'a' :
             vel.linear.x = 0.1
-            vel.angular.z = 
+            vel.angular.z = -0.1
         elif key =='w' :
             vel.linear.x = 0.1
-            vel.angular.z =
+            vel.angular.z = 0.0
         elif key == 'd' :
             vel.linear.x = 0.1
-            vel.angular.z = 
+            vel.angular.z = 0.1
         else:
             vel.linear.x = 0.0
             vel.angular.z = 0.0
+        return vel
+
+    def drive(self, vel):    
+        self.vel_pub.publish(vel)
         
-        self.publisher.publish(vel)
-        
-    def getKey():
+    def getKey(self, settings):
         tty.setraw(sys.stdin.fileno())
         select.select([sys.stdin], [], [], 0)
         key = sys.stdin.read(1)
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
         return key
 
-    settings = termios.tcgetattr(sys.stdin)
-    key = None
 
-    while key != '\x03':
-        key = getKey()
+    def run_loop (self):
+        settings = termios.tcgetattr(sys.stdin)
+        key = None
+        key = self.getKey(settings)
         print(key)
+        vel = self.direction(key)
+        self.drive(vel)
 
-
-def main():
+def main(args = None):
     rclpy.init(args=args) 
     node = TeleopNode()
-
+    rclpy.spin(node)
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
